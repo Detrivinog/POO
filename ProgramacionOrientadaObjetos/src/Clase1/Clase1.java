1b40ee8c0813baa71af744ba89d6189d3e7e1f85
Detrivinog <detrivinog@gmail.com>
2021-10-19 20:01:48 -0500

    Clase 1: introduccion

---
+++ ProgramacionOrientadaObjetos/src/Clase1/Clase1.java
@@ -0,0 +1,13 @@
+package Clase1;
+
+public class Clase1 {
+    //Método main
+    public static void main(String[] args) {
+        String nombre = "Braulio";
+        String color = "Blanco";
+        double produccion = 1250.40;
+        String raza = "Criolla";
+
+        System.out.println("Nombre: "+nombre+", Color: "+color+", Producción: "+produccion+", Raza: "+raza);
+    }
+}
